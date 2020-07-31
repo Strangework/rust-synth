079e4c3e108812ccbc89b7c5c2f6d18000f2bfe0
Nicholas Yang Chee <nicholas.yang.chee@gmail.com>
2020-07-31 00:11:17 -0400

    Started implementation of JACK client, and changed Wave class to export f32 wave files instead of uint32

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod wave;
+use jack;
 
 fn main() {
+    let (client, status) =
+      jack::Client::new("tone-gen", jack::ClientOptions::NO_START_SERVER).unwrap();
+    println!("{} : {:?}", client.name(), status);
+
+    let mut port = client
+      .register_port("tone-out", jack::AudioOut::default())
+      .unwrap();
+ 
     let freq = 2000.0;
-    let w = wave::Wave::tone(freq, 0.8, 1.0, 44_100);
+    let sample_rate = client.sample_rate() as u32;
+    let w = wave::Wave::tone(freq, 0.8, 0.8, sample_rate);
     //let w = wave::Wave::silence(3.0, 44_100);
     println!("{:?}", w.samples.len());
     let export_path = format!("{}.wav", freq.to_string());
--- src/wave.rs
+++ src/wave.rs
@@ -1,26 +1,26 @@
 use std::f64::consts::PI;
-use std::i16;
 use hound;
 
 pub struct Wave {
     duration: f64,
     spec: hound::WavSpec,
-    pub samples: Vec<i16>
+    pub samples: Vec<f32>
 }
 
 impl Wave {
     pub fn silence(duration: f64, sample_rate: u32) -> Wave {
+        // !!: How can this be initialized outside of this function?
         let spec = hound::WavSpec {
             channels: 1,
             sample_rate,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
+            bits_per_sample: 32,
+            sample_format: hound::SampleFormat::Float,
         };
         let sample_count: usize = (duration * sample_rate as f64) as usize;
         Wave {
             duration,
             spec,
-            samples: vec![0; sample_count],
+            samples: vec![0.0; sample_count],
         } 
     }
 
@@ -28,14 +28,14 @@ impl Wave {
         let spec = hound::WavSpec {
             channels: 1,
             sample_rate,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
+            bits_per_sample: 32,
+            sample_format: hound::SampleFormat::Float,
         };
         let sample_count: usize = (duration * sample_rate as f64) as usize;
         let mut samples = Vec::new();
         for n in (0 .. sample_count).map(|x| x as f64 / sample_count as f64) {
-            let sample = (n * freq * 2.0 * PI).sin() * i16::MAX as f64 * amp;
-            samples.push(sample as i16);
+            let sample = (n * freq * 2.0 * PI).sin() * 1 as f64 * amp;
+            samples.push(sample as f32);
         }
  
         Wave {
@@ -50,6 +50,6 @@ impl Wave {
         // TODO : Implement channel interleaving
         for sample in self.samples.iter() {
             writer.write_sample(*sample).unwrap();
-        } 
+        }
     }
 }
